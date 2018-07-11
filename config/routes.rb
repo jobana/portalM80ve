ea3a116d9052f3c25f281166837e2802a730c8ef
jobana <nellyjobana@gmail.com>
2018-07-11 15:56:25 -0500

    precompilacion de assets

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'landing/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root :to => 'landing#index'
+  root :to => 'contacts#new'
 
   match '/contacts',     to: 'contacts#new',             via: 'get'
 resources :contacts, only: [:new, :create]
